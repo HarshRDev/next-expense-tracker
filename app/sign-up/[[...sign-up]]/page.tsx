1dbf6c6c313784136cb0dc3bcae3b2e884dfe61a
HarshRDev <h.rungta91@gmail.com>
2025-11-18 23:36:50 +0530

    Complete Navbar & Components

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function SignUpPage() {
-  return <SignUp />;
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+  <SignUp />
+  </div>
+  )
 }

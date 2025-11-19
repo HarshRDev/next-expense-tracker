cd3dc5d92e20ecdd8337f6fdb9ac372b501cd7fb
HarshRDev <h.rungta91@gmail.com>
2025-11-20 01:02:01 +0530

    Add expense chart to the dashboard

--- components/AddNewRecord.tsx
+++ components/AddNewRecord.tsx
@@ -237,8 +237,8 @@ const AddRecord = () => {
               </span>
             </label>
             <div className='relative'>
-              <span className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400 font-medium text-sm'>
-                Rs.
+              <span className='absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400 font-medium text-sm'>
+                Rs
               </span>
               <input
                 type='number'
